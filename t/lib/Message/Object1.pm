77743b4be3605cb77c0bd9fa00e303b679494d36
Simon Adams <simonadams@gmail.com>
2015-02-13 20:33:48 +0000

    fixed obj_test.pl

--- t/lib/Message/Object1.pm
+++ t/lib/Message/Object1.pm
@@ -17,7 +17,7 @@ has 'type' =>    (is => 'ro', isa => 'header_int', default => '1', traits => ["D
 
 has 'o1_uint16_1' => (is => 'rw', isa => 'uint16', traits => ["DataType"], data_type => Types::PRIMITIVE);
 has 'o1_obj_c1' => (is => 'rw', isa => 'Message::Object1_Child1', traits => ["DataType"], data_type => Types::OBJECT);
-has 'o1_obj_c2_h' => (is => 'rw', isa => 'HashRef', traits => ["Mapped","DataType"], key_types => ["uint32", "Message::Object1_Child2"], data_type => Types::OBJECT);
+has 'o1_obj_c2_h' => (is => 'rw', isa => 'HashRef', traits => ["Mapped","DataType"], key_types => ["uint32", "Message::Object1_Child2"], data_type => Types::MAP);
 has 'o1_uint16_2' => (is => 'rw', isa => 'uint16', traits => ["DataType"], data_type => Types::PRIMITIVE);
 
 __PACKAGE__->meta->make_immutable();
